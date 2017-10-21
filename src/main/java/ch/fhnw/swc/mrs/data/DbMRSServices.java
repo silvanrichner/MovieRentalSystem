4ca6180866a640fb8ad298d7103dcf1dace6d929
Silvan <silvan.richner@gmx.ch>
2017-10-21 10:42:16 +0200

    DB Test
    prepared db testing, modified gitignore

--- src/main/java/ch/fhnw/swc/mrs/data/DbMRSServices.java
+++ src/main/java/ch/fhnw/swc/mrs/data/DbMRSServices.java
@@ -9,7 +9,7 @@ import ch.fhnw.swc.mrs.model.Rental;
 import ch.fhnw.swc.mrs.model.User;
 
 public class DbMRSServices implements MRSServices {
-    private static final String DB_CONNECTION = "jdbc:hsqldb:file:mrs";
+    private static final String DB_CONNECTION = "jdbc:hsqldb:file:src/main/resources/mrs";
 	private Database db;
 
 	private MovieDAO getMovieDAO() { 
